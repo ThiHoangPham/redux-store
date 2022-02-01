b7c210c50179b4a9fb594de3f2281cbb8d4547a4
Adam Pham <thaihoangpham2008@gmail.com>
2022-02-01 22:35:58 +0800

    finished Cart/index.js, refactoring CartItem/index.js

--- client/src/components/Cart/index.js
+++ client/src/components/Cart/index.js
@@ -11,99 +11,101 @@ import { useLazyQuery } from '@apollo/react-hooks';
 
 import { useDispatch, useSelector } from 'react-redux';
 
-const stripePromise = loadStripe('pk_test_TYooMQauvdEDq54NiTphI7jx');
+const stripePromise = loadStripe('pk_test_TYooMQauvdEDq54NiTphI7jx')
 
 const Cart = () => {
-  const [state, dispatch] = useStoreContext();
-  const [getCheckout, { data }] = useLazyQuery(QUERY_CHECKOUT);
-
-  useEffect(() => {
-    if (data) {
-      stripePromise.then((res) => {
-        res.redirectToCheckout({ sessionId: data.checkout.session });
-      });
-    }
-  }, [data]);
-
-  useEffect(() => {
-    async function getCart() {
-      const cart = await idbPromise('cart', 'get');
-      dispatch({ type: ADD_MULTIPLE_TO_CART, products: [...cart] });
-    }
-
-    if (!state.cart.length) {
-      getCart();
-    }
-  }, [state.cart.length, dispatch]);
-
-  function toggleCart() {
-    dispatch({ type: TOGGLE_CART });
-  }
-
-  function calculateTotal() {
-    let sum = 0;
-    state.cart.forEach((item) => {
-      sum += item.price * item.purchaseQuantity;
+    const state = useSelector((state) => {
+        return state;
     });
-    return sum.toFixed(2);
-  }
+    const dispatch = useDispatch();
 
-  function submitCheckout() {
-    const productIds = [];
+    const[getCheckout, { data }] = useLazyQuery(QUERY_CHECKOUT);
 
-    state.cart.forEach((item) => {
-      for (let i = 0; i < item.purchaseQuantity; i++) {
-        productIds.push(item._id);
-      }
-    });
+    useEffect(() => {
+        // async function to get data from IndexedDB
+        async function getCart() {
+            const cart = await idbPromise('cart', 'get');
+            dispatch({ type: ADD_MULTIPLE_TO_CART, products: [...cart] });
+        };
+        // check global state for any cart products, and if not, use function to retrieve data from the IndexedDB store
+        if (!state.cart.length) {
+            getCart();
+        }
+    }, [state.cart.length, dispatch]);
 
-    getCheckout({
-      variables: { products: productIds },
-    });
-  }
+    useEffect(() => {
+        if(data) {
+            stripePromise.then((res) => {
+                res.redirectToCheckout({ sessionId: data.checkout.session })
+            });
+        }
+    }, [data]);
 
-  if (!state.cartOpen) {
+    function toggleCart() {
+        dispatch({ type: TOGGLE_CART });
+    }
+
+    if (!state.cartOpen) {
+        return (
+            <div className="cart-closed" onClick={toggleCart}>
+                <span role="img" aria-label="cart">🛒</span>
+            </div>
+        )
+    }
+
+    function calculateTotal() {
+        let sum = 0;
+        state.cart.forEach(item => {
+            sum += item.price * item.purchaseQuantity;
+        });
+        return sum.toFixed(2);
+    }
+
+    function submitCheckout() {
+        const productIds = [];
+
+        getCheckout({
+            variables: { products: productIds }
+        });
+
+        state.cart.forEach((item) => {
+            for (let i = 0; i < item.purchaseQuantity; i++) {
+                productIds.push(item._id)
+            }
+        });
+    }
+
     return (
-      <div className="cart-closed" onClick={toggleCart}>
-        <span role="img" aria-label="trash">
-          🛒
-        </span>
-      </div>
-    );
-  }
-
-  return (
-    <div className="cart">
-      <div className="close" onClick={toggleCart}>
-        [close]
-      </div>
-      <h2>Shopping Cart</h2>
-      {state.cart.length ? (
-        <div>
-          {state.cart.map((item) => (
-            <CartItem key={item._id} item={item} />
-          ))}
-
-          <div className="flex-row space-between">
-            <strong>Total: ${calculateTotal()}</strong>
-
-            {Auth.loggedIn() ? (
-              <button onClick={submitCheckout}>Checkout</button>
+        <div className="cart">
+            <div className="close" onClick={toggleCart}>[close]</div>
+            <h2>Shopping Cart</h2>
+            {state.cart.length ? (
+                <div>
+                {state.cart.map(item => (
+                    <CartItem key={item._id} item={item} />
+                ))}
+                <div className="flex-row space-between">
+                    <strong>Total: ${calculateTotal()}</strong>
+                    {
+                    Auth.loggedIn() ?
+                        <button onClick={submitCheckout}>
+                        Checkout
+                        </button>
+                        :
+                        <span>(log in to check out)</span>
+                    }
+                </div>
+                </div>
             ) : (
-              <span>(log in to check out)</span>
+                <h3>
+                <span role="img" aria-label="shocked">
+                    😱
+                </span>
+                You haven't added anything to your cart yet!
+                </h3>
             )}
-          </div>
         </div>
-      ) : (
-        <h3>
-          <span role="img" aria-label="shocked">
-            😱
-          </span>
-          You haven't added anything to your cart yet!
-        </h3>
-      )}
-    </div>
-  );
+    );
 };
 
 export default Cart;
--- client/src/components/CartItem/index.js
+++ client/src/components/CartItem/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import { useStoreContext } from "../../utils/GlobalState";
-import { REMOVE_FROM_CART, UPDATE_CART_QUANTITY } from "../../utils/actions";
-import { idbPromise } from "../../utils/helpers";
+import { REMOVE_FROM_CART, UPDATE_CART_QUANTITY } from '../../utils/actions';
+import { idbPromise } from '../../utils/helpers';
+import { useDispatch } from 'react-redux';
 
 const CartItem = ({ item }) => {
 
