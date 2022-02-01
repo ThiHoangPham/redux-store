1d8f6f0c0a36d357a42578df5c674d7ca459d97f
Adam Pham <thaihoangpham2008@gmail.com>
2022-02-01 22:13:00 +0800

    working on muatation & queries

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,4 +1,4 @@
-import { gql } from '@apollo/client';
+import gql from 'graphql-tag';
 
 export const LOGIN = gql`
   mutation login($email: String!, $password: String!) {
@@ -11,37 +11,29 @@ export const LOGIN = gql`
   }
 `;
 
+
 export const ADD_ORDER = gql`
   mutation addOrder($products: [ID]!) {
     addOrder(products: $products) {
       purchaseDate
       products {
         _id
+      name
+      description
+      price
+      quantity
+      category {
         name
-        description
-        price
-        quantity
-        category {
-          name
-        }
+      } 
       }
     }
   }
 `;
 
+
 export const ADD_USER = gql`
-  mutation addUser(
-    $firstName: String!
-    $lastName: String!
-    $email: String!
-    $password: String!
-  ) {
-    addUser(
-      firstName: $firstName
-      lastName: $lastName
-      email: $email
-      password: $password
-    ) {
+  mutation addUser($firstName: String!, $lastName: String!, $email: String!, $password: String!) {
+    addUser(firstName: $firstName, lastName: $lastName, email: $email, password: $password) {
       token
       user {
         _id
--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,4 +1,4 @@
-import { gql } from '@apollo/client';
+import gql from 'graphql-tag';
 
 export const QUERY_PRODUCTS = gql`
   query getProducts($category: ID) {
@@ -12,14 +12,6 @@ export const QUERY_PRODUCTS = gql`
       category {
         _id
       }
-    }
-  }
-`;
-
-export const QUERY_CHECKOUT = gql`
-  query getCheckout($products: [ID]!) {
-    checkout(products: $products) {
-      session
     }
   }
 `;
@@ -40,31 +32,39 @@ export const QUERY_ALL_PRODUCTS = gql`
 `;
 
 export const QUERY_CATEGORIES = gql`
-  {
-    categories {
-      _id
-      name
-    }
+{
+  categories {
+    _id
+    name
   }
+}
 `;
 
 export const QUERY_USER = gql`
-  {
-    user {
-      firstName
-      lastName
-      orders {
+{
+  user {
+    firstName
+    lastName
+    orders {
+      _id
+      purchaseDate
+      products {
         _id
-        purchaseDate
-        products {
-          _id
-          name
-          description
-          price
-          quantity
-          image
-        }
+        name
+        description
+        price
+        quantity
+        image
       }
+    }
+  }
+}
+`;
+
+export const QUERY_CHECKOUT = gql`
+  query getCheckout($products: [ID]!) {
+    checkout(products: $products) {
+      session
     }
   }
 `;
